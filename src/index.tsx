6fbd5e4f87eb5b4397e7354de6e1faaf3e7afc03
应凯利 <ying_kaili@finsung.com>
2022-01-13 10:27:33 +0800

    feat: 完成历史记录回放

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Interface } from 'readline';
 import './index.css'
 
 interface SquarePropsType{
@@ -50,10 +49,8 @@ class Square extends React.Component<SquarePropsType,StateType> {
     }
     
     render() {
-        
       return (
         <div>
-          
           <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
@@ -86,11 +83,12 @@ class Square extends React.Component<SquarePropsType,StateType> {
     
     history:History[]
     XIsNext:boolean;
+    stepNumber: number
   }
   class Game extends React.Component<GameProps,GameState> {
     handleClick(i:number){
-        const history = this.state.history;
-        const current = history[history.length-1];
+        const history = this.state.history.slice(0,this.state.stepNumber +1);
+        const current = history[this.state.stepNumber];
         const squares = current.squares.slice();
         if(calculateWinner(squares) || squares[i]){
             return;
@@ -99,21 +97,40 @@ class Square extends React.Component<SquarePropsType,StateType> {
         this.setState({history:history.concat([
             {
                 squares:squares
-            }
-        ]),XIsNext:!this.state.XIsNext})
+            },
+            
+        ]),
+        stepNumber:history.length,
+        XIsNext:!this.state.XIsNext})
         
     }
+    jumpTo(step:number) {
+        this.setState({
+          stepNumber: step,
+          XIsNext: (step % 2) === 0,
+        });
+      }
     constructor(props:GameProps){
         super(props)
         this.state = {
-            history: Array(9).fill(''),
+            history: [{squares:[]}],
             XIsNext:true,
+            stepNumber:0,
         }
     }
     render() { 
-    const history = this.state.history;
-    const current = history[history.length-1];
+    const history = this.state.history; 
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step,move)=>{
+        const desc = move ? 'Go to move #' + move : 'Go to game start';
+        return (
+            <li key={move}>
+                <button onClick={()=>this.jumpTo(move)}>{desc}</button>
+            </li>
+        )
+    })
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
@@ -127,7 +144,7 @@ class Square extends React.Component<SquarePropsType,StateType> {
           </div>
           <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
